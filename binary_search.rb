ed46b305669b670915869f4b6ed573349d6b01a9
Marcin Galiński <marcin.galinski95@gmail.com>
2016-01-03 12:53:09 +0100

    refactor code
    create Tree class for better clarity

--- binary_search.rb
+++ binary_search.rb
@@ -2,76 +2,87 @@ class Node
 	attr_reader :value
 	attr_accessor(:parent, :l_child, :r_child)
 
-	def self.build_tree ary
-		return if ary.empty?
-		root = self.new ary.pop 
-		ary.each do |number|
-			self.place_node(number, root)
-		end
-		root
-	end
-
 	def initialize(value, parent=nil)
 		@value = value
 		@parent = parent
 	end
+end
+
+class Tree < Node
+	def initialize(ary)
+		root = build_tree(ary)
+		@parent = nil
+		@value = root.value
+		@l_child = root.l_child
+		@r_child = root.r_child
+	end
+
+	def breadth_first_search target
+		queue = [self]
+
+		while current = queue.shift
+			return current if current.value == target
+			queue << current.l_child if current.l_child
+			queue << current.r_child if current.r_child 
+		end
+	end
+
+	def depth_first_search target
+		stack = [self]
+		checked = []
+
+		while current = stack.last
+			return current if current.value == target
+			checked << current
+			if current.l_child && !checked.include?(current.l_child)
+				stack << current.l_child
+			elsif current.r_child && !checked.include?(current.r_child)
+				stack << current.r_child
+			else
+				stack.pop
+			end
+		end
+	end
+
+	def dfs_rec(target, current=self)
+		result = current if current.value == target
+		result ||= dfs_rec(target, current.l_child) if current.l_child
+		result ||= dfs_rec(target, current.r_child) if current.r_child
+		result
+	end
 
 	private
 
-	def self.place_node(number, parent_node)
+	def build_tree ary
+		return if ary.empty?
+		root = Node.new ary.pop 
+		ary.each do |number|
+			place_node(number, root)
+		end
+		root
+	end
+
+	def place_node(number, parent_node)
 		if parent_node.value > number
 			if parent_node.l_child
-				self.place_node(number, parent_node.l_child)
+				place_node(number, parent_node.l_child)
 			else
-				new_node = self.new(number, parent_node)
+				new_node = Node.new(number, parent_node)
 				parent_node.l_child = new_node
 			end
 		else
 			if parent_node.r_child
-				self.place_node(number, parent_node.r_child)
+				place_node(number, parent_node.r_child)
 			else
-				new_node = self.new(number, parent_node)
+				new_node = Node.new(number, parent_node)
 				parent_node.r_child = new_node
 			end
 		end
 	end
 end
 
-def breadth_first_search(target, root)
-	queue = [root]
-
-	while current = queue.shift
-		return current if current.value == target
-		queue << current.l_child if current.l_child
-		queue << current.r_child if current.r_child 
-	end
-end
-
-def depth_first_search(target, root)
-	stack = [root]
-	checked = []
-
-	while current = stack.last
-		return current if current.value == target
-		checked << current
-		if current.l_child && !checked.include?(current.l_child)
-			stack << current.l_child
-		elsif current.r_child && !checked.include?(current.r_child)
-			stack << current.r_child
-		else
-			stack.pop
-		end
-	end
-end
-
-def dfs_rec(target, current)
-	result = current if current.value == target
-	result ||= dfs_rec(target, current.l_child) if current.l_child
-	result ||= dfs_rec(target, current.r_child) if current.r_child
-	result
-end
-
 array = Array.new(50) { |i| i }
-root = Node.build_tree array.shuffle
-node = dfs_rec(array.sample, root)
-puts "#{node} parent: #{node.parent} children: #{node.l_child} #{node.r_child}"
+tree = Tree.new array.shuffle
+puts tree.breadth_first_search(array.sample)
+puts tree.depth_first_search(array.sample)
+puts tree.dfs_rec(array.sample)
